5c2b7d59fbf1df715936becfa277ded60232e46f
artemgur <artemgur01@gmail.com>
2021-05-11 20:13:41 +0300

    Improvements

--- dotnettest/Article.cs
+++ dotnettest/Article.cs
@@ -1,24 +1,49 @@
+using System;
+
 namespace dotnettest
 {
     public class Article
     {
-        public string Name;
-        public string Text;
-        public string ReviewerComment = null;
-        public bool IsReviewed = false;
-        public bool IsAccepted = false;
-        public bool IsPublished = false;
+        public string Name { get; private set; }
+        public string Text { get; private set; }
+        //Temporary field
+        public string ReviewerComment { get; private set; }
+        public bool IsReviewed { get; private set; }
+        public bool IsAccepted { get; private set; }
+        public bool IsPublished { get; private set; }
 
         public Article(string name, string text)
         {
             Name = name;
-            text = text;
+            Text = text;
+            IsPublished = false;
+            ReviewerComment = null;
+            IsReviewed = false;
+            IsAccepted = false;
+            IsPublished = false;
         }
 
         public void Review(bool isAccepted, string review)
         {
+            if (IsPublished)
+                throw new InvalidOperationException();
+            IsReviewed = true;
             IsAccepted = isAccepted;
             ReviewerComment = review;
+        }
+
+        public void ChangeText(string newText)
+        {
+            if (IsAccepted || IsPublished)
+                throw new InvalidOperationException();
+            Text = newText;
+        }
+
+        public void Publish()
+        {
+            if (!IsAccepted)
+                throw new InvalidOperationException();
+            IsPublished = true;
         }
     }
 }
